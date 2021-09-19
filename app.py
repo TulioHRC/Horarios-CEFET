8ecc7405cbbacfc02e755d147dfb58aab8bd02de
Grove <grove1411@gmail.com>
2021-09-19 19:46:31 -0300

    Tabs on adding info

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 def go():
     print('Criando novos horários...')
@@ -10,6 +11,7 @@ class MainApp:
         self.sizes = [self.master.winfo_screenwidth(), self.master.winfo_screenheight()]
         self.master.geometry(f"{int(self.sizes[0]*0.8)}x{int(self.sizes[1]*0.8)}+{int(self.sizes[0]*0.1)}+{int(self.sizes[1]*0.1)}")
         self.master.resizable(0,0)
+        self.master.config(bg="Black")
 
         self.bt = Button(self.master, text="Criar horários", command=go, font=('Arial', 26)) # Rodará o código principal
         self.bt.config(bg="Gray", fg="White")
@@ -17,11 +19,31 @@ class MainApp:
         self.bt.place(bordermode=OUTSIDE, width=str(int(self.sizes[0]*0.8*0.4)), height=str(int(self.sizes[1]*0.8*0.3)),
                             relx=0.3, rely=0.35)
 
-        self.new = Button(self.master, text="Novos dados", font=('Arial', 20)) # Novos dados (adicionados lá no excel)
+        self.new = Button(self.master, text="Novos dados", command=AddingData, font=('Arial', 20)) # Novos dados (adicionados lá no excel)
         self.new.config(bg="Gray", fg="White")
         self.new.pack()
         self.new.place(bordermode=OUTSIDE, width=str(int(self.sizes[0]*0.8*0.2)), height=str(int(self.sizes[1]*0.8*0.1)),
                             relx=0.05, rely=0.85)
+
+class AddingData(MainApp):
+    def __init__(self):
+        self.screen = Toplevel()
+        self.screen.title('Adicionando dados')
+        self.sizes = [self.screen.winfo_screenwidth(), self.screen.winfo_screenheight()]
+        self.screen.geometry(f"{int(self.sizes[0]*0.6)}x{int(self.sizes[1]*0.6)}+{int(self.sizes[0]*0.2)}+{int(self.sizes[1]*0.2)}")
+        self.screen.resizable(0,0)
+
+        self.tabs = ttk.Notebook(self.screen)
+        self.tabs.pack(fill=BOTH, expand=True)
+
+        self.addTeacher = Frame(self.tabs, width=int(self.sizes[0]*0.6), bg="Black")
+        self.addRoom = Frame(self.tabs, width=int(self.sizes[0]*0.6), bg="Black")
+
+        self.addTeacher.pack(fill=BOTH, expand=True)
+        self.addRoom.pack(fill=BOTH, expand=True)
+
+        self.tabs.add(self.addTeacher, text="Nova matéria/professor")
+        self.tabs.add(self.addRoom, text="Nova sala")
 
 
 def main():
