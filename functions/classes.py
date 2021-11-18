63ee3455fbceb50e5f6a41a1d0502693cf8dd45b
Grove <grove1411@gmail.com>
2021-11-17 22:45:17 -0200

    Second Commig

--- functions/classes.py
+++ functions/classes.py
@@ -1,42 +1,43 @@
 class Teacher:
-    def __init__(self, name, subject, year, prefers, limits, classes):
+    def __init__(self, name, subject, prefers, limits, classes):
         self.name = name
-        self.subject = subject
-        self.year = year
-        self.prefers = prefers
+        self.subjects = [subject] # Formato = ['{subject}:{year}']
+        self.prefers = [prefers] # Formato = ['{subject}:{prefers}']
         self.limits = limits
-        self.classes = classes # Formato = ['{turma}-{número de horários}', ...]
+        self.classes = [classes] # Formato = [['{turma}{ano}-{número de horários}', ...]] Uma lista pra cada matéria
 
-    def bestHour(self, turm, preDefinedHour="", alreadyChose=""): # Definição do melhor horário para o professor
+    def bestHour(self, turm, subject, preDefinedHour="", alreadyChose=""): # Definição do melhor horário para o professor
 
         # Formato dos argumentos: class="{turma}"
         #           preDefinedHour="{day}-{horario de 0 (7:50-8:40) até 9 (16:40-17:30)}"
-        #           alreadyChose={2: '{números dos horários já escolhidos}', ... (outros dias, até o 6)}
+        #           alreadyChose={'2': '{lista com os números dos horários já escolhidos}', ... (outros dias, até o 6)}
 
         if preDefinedHour and not f"N{preDefinedHour[0]}" in limits and not f"N{preDefinedHour[0]}" in prefers:
             # Se o horário pré-definido poder (não for proibido pelas limits ou não for preferido)
             return preDefinedHour
 
-        if f'{turm}-0' in self.classes: return 0 # Se não houver horários da turma
+        if f'{turm.name[0:-3]}-0' in self.classes: return 0 # Se não houver horários da turma
 
         # Pegando o melhor horário possível para o professor
         goodOptions = 0
-        for prefer in self.prefers.split('-'):
-            if 'S' in prefer and prefer[-1] in alread
+        for prefer in self.prefers[self.subjects.index(subject)].split(":")[1].split('-'):
+            if 'S' in prefer and alreadyChose[f"{prefer[1]}"]:
+                for hour in alreadyChose[f"{prefer[1]}"]:
+                    #...
 
 ############# Falta forma de se pegar os "pontos" que o Samuel falou, ou seja, avaliação de cada possibilidade com um valor dependendo das suas
 # conveniencias e dificuldades
 
 class Turm:
-    def __init__(self, name):
-        self.name = name
-        self.schedule1 = { # Horários de cada turma
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': [],
-        }
-        self.schedule2 = self.schedule1.copy() # 2 ano
-        self.schedule3 = self.schedule1.copy() # 3 ano
+    def __init__(self, name, year, preDefinedSchedule={'2': [],'3': [],'4': [],'5': [],'6': []}):
+        self.name = f'{name}-{year}A'
+        self.schedule = preDefinedSchedule # Horários de cada turma
         # Formato dos horários = '{horário de 0 a 9}-{matéria/subject}'
+
+
+class Room:
+    def __init__(self, name, position, limits, preDefinedSchedule={'2': [],'3': [],'4': [],'5': [],'6': []}):
+        self.name = name
+        self.position = position
+        self.limits = limits
+        self.schedule = preDefinedSchedule # Horários de cada sala
--- functions/go.py
+++ functions/go.py
@@ -8,7 +8,7 @@ import classes as c
 
 def mainFunction(): # A função principal do código, que retornará o resultado que nós esperamos
 
-    # Pegando os dados que nós fornecemos
+    # =================== Pegando os dados que nós fornecemos
     try:
         teachersData = loadData.getDatabase('Planilha.xlsx')
         teachersColumns = loadData.getDatabase('Planilha.xlsx', get="columns")
@@ -16,31 +16,49 @@ def mainFunction(): # A função principal do código, que retornará o resultad
     except Exception as e:
         print(f"Houve um erro ao tentar pegar os dados das planilhas.\n{e}")
 
-    # Processando os dados para deixa-los melhor de mexer
+    # ==================== Processando os dados para deixa-los melhor de mexer
 
-    classes = [] # Turmas
+    classes = [] # -- Turmas
 
     for i in range(5, len(teachersColumns)): # Pega apenas as matérias
         classes.append(teachersColumns[i])
 
     for index, turm in enumerate(classes): # Transforma cada turma em um objeto de uma classe
-        classes[index] = c.Turm(turm)
+        for i in range(1, 4): # Turma para cada ano
+            classes[index] = c.Turm(turm, str(i))
 
-    teachers = [] # Lista de objetos (professores)
+    teachers = [] # -- Lista de objetos (professores)
+    teachersNames = []
 
     for index, teacher  in enumerate(teachersData["Professor"]): # Transforma cada professor em um objeto de uma classe
         # Aulas por professor em cada turma
         teachersClasses = []
         for turm in classes:
-            teachersClasses.append(f'{turm.name}-{int(teachersData[f"{turm.name}"][index])}')
+            teachersClasses.append(f'{turm.name}-{int(teachersData[f"{turm.name[0:-3]}"][index])}')
 
-        teachers.append(c.Teacher(teacher, teachersData["Materia"][index], teachersData["Ano"][index], teachersData["Preferencias"][index], teachersData["Limitacoes"][index], teachersClasses))
+        if not teacher in teachersNames:
+            teachers.append(c.Teacher(teacher, f'{teachersData["Materia"][index]}:{teachersData["Ano"][index]}', f'{teachersData["Materia"][index]}:{teachersData["Preferencias"][index]}', teachersData["Limitacoes"][index], teachersClasses))
+            teachersNames.append(teacher)
+        else:
+            teachers[teachersNames.index(teacher)].subjects.append(f'{teachersData["Materia"][index]}:{teachersData["Ano"][index]}')
+            teachers[teachersNames.index(teacher)].prefers.append(f'{teachersData["Materia"][index]}:{teachersData["Preferencias"][index]}')
+            teachers[teachersNames.index(teacher)].limits  = teachersData["Limitacoes"][index] # Substitui pela mais recente
+            teachers[teachersNames.index(teacher)].classes.append(teachersClasses)
 
-    # Gerando o resultado dos melhores horários
+        # print(f'{teachers[teachersNames.index(teacher)].name}, {teachers[teachersNames.index(teacher)].subjects}\n{teachers[teachersNames.index(teacher)].classes}')
+
+    rooms = [] # -- Salas
+
+    for index, room in enumerate(roomsData["Sala"]):
+        rooms.append(c.Room(room, roomsData["Local"][index], roomsData["Limitacoes"][index]))
+
+    # ====================== Gerando o resultado dos melhores horários
 
     for teacher in teachers:
-        for turm in classes:
-            teacher.bestHour(turm.name) # Melhor horário para cada turma
-
+        for subject in teacher.subjects: # Por matéria do professor
+            for turm in classes:
+                resp = teacher.bestHour(turm, subject, alreadyChose=turm.schedule) # Melhor horário para cada turma
+                #print(resp)
+                if resp == 0: continue # Pula professor, não tem aula nessa matéria
 
 mainFunction()
