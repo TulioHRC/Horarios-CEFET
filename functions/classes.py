b3f405e992fd58e914d8b6632bc8fb45a613ca0a
Samuel Leão Lopes <samukallopes02@gmail.com>
2022-01-24 11:55:11 -0300

    Dia 24

--- functions/classes.py
+++ functions/classes.py
@@ -5,22 +5,44 @@ import re
 class Teacher:
     def __init__(self, name, subject, type, prefers, limits, horaries=""):
         self.name = name
-        self.subjects = [subject] # Formato = cada matéria será adicionada nesta lista, com a seguinte formatação nome:turma, ex.: Matematica:MCT-2A
-        self.types = [type] # Formato = semelhante ao de cima, tendo as opções: manha, tarde ou noite
-        self.prefers = [prefers] # Formato = haverá uma lista para cada uma das matérias, tendo o seguinte formato S ou N dia:horário inicial - horário final, ex.: S6:2-4, ou seja,
-        # prefere dar aula na sexta entre os horários 2 e 4
-        self.limits = [limits] # Formato = segue o mesmo do acima, tendo a formatação N dia:horário inicial - horário final, ex.: N5:3-5, ou seja, não pode quinta nos horários 3 a 5
-        self.schedule = {'2': [],'3': [],'4': [],'5': [],'6': []} # Horários durante a semana do professor
-        self.horaries = {} # Formato = {subject: {Turm: classesInTheTurm}}, ex.: {'Matematica': {MCT-1A: 4, ...}}
+
+        # Formato = cada matéria será adicionada nesta lista, com a seguinte formatação nome:turma,ex.:Matematica:MCT-2A
+        self.subjects = [subject]
+
+        # Formato = semelhante ao de cima, tendo as opções: manha, tarde ou noite
+        self.types = [type]
+
+        # Formato = haverá uma lista para cada uma das matérias, tendo o seguinte formato S ou N dia:horário inicial
+        # - horário final, ex.: S6:2-4, ou seja, prefere dar aula na sexta entre os horários 2 e 4
+        self.prefers = [prefers]
+
+        # Formato = segue o mesmo do acima, tendo a formatação N dia:horário inicial - horário final, ex.: N5:3-5,
+        # ou seja, não pode quinta nos horários 3 a 5
+        self.limits = [limits]
+
+        # Horários durante a semana do professor
+        self.schedule = {'2': [], '3': [], '4': [], '5': [], '6': []}
+
+        # Formato = {subject: {Turm: classesInTheTurm}}, ex.: {'Matematica': {MCT-1A: 4, ...}}
+        self.horaries = {}
         self.horaries[subject.split('-')[0]] = horaries
-        self.classes = 0 # Número de aulas totais que está lecionando
+
+        # Quantidade de objetos "Horários" que o professor tem -> total de aulas que ele dá na semana
+        self.classes = 0
+
+        # Todos os objetos "Horários" relacionados a esse professor
         self.h_individuais = []
+
+
+    def __repr__(self):
+        return f'{self.name}'
 
 
 class Turm:
     def __init__(self, name, year, group):
-        self.name = f'{name}-{year}{group}' # ex.: MEC-3B
-        self.schedule = {'2': [],'3': [],'4': [],'5': [],'6': []} # Horários durante a semana da turma
+        self.name = f'{name}-{year}{group}'  # ex.: MEC-3B
+        self.schedule = {'2': [], '3': [], '4': [], '5': [], '6': []}  # Horários durante a semana da turma
+
 
 """ Ainda não foi adicionada
 class Room:
@@ -34,13 +56,14 @@ class Room:
             self.schedule = {'2': [],'3': [],'4': [],'5': [],'6': []}
 """
 
+
 class Horario:
     def __init__(self, teacher, subject, turm, possible_h=0):
         self.teacher = teacher
         self.subject = subject
         self.turm = turm
         self.possible_h = possible_h
-        #self.position = None
+        # self.position = None
 
     def __repr__(self):
         return f'{self.teacher}-{self.subject}'
---
+++ functions/logic.py
@@ -0,0 +1,7 @@
+# Arquivo com as funções envolvendo a lógica do nosso programa
+
+def getBetterHour()
+
+def cost()
+
+def validation(horario)
