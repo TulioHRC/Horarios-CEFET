0f916fb9027f3c0d49a40bb78d91a12cd5a9c169
Samuel Leão Lopes <samukallopes02@gmail.com>
2022-02-22 16:28:08 -0300

    Adaptação para 6 horários e para horários Bimestrais: Samu

--- TESTE.py
+++ TESTE.py
@@ -1,9 +1,4 @@
 lista = []
-impar = [1, 3, 5, 7, 9]
-par = [0, 2, 4, 6, 8, 10]
-
-for c in range(0, 5):
-    for d in range(0, 3) if (c in par) else range(10, 13):
-        lista.append(d)
-
-print(lista)
+if str(type(lista)) == "<class 'list'>":
+    print('Sim')
+print(str(type(lista)))
